63d4c4e83c741debc71a1d8720ffd5d1d997c54d
trayveonte <tnichols456@gmail.com>
2016-11-18 11:26:50 -0800

    Overlap added

--- script.js
+++ script.js
@@ -2,12 +2,31 @@ var namespace = "http://www.w3.org/2000/svg"
 
 var flyingSaucer = document.getElementById("ufo");
 var fs = 500;
-var fy = 250
+var fy = 250;
+
+function randomNumber(min,max) {
+ return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+var foodEaten = 0;
+var textArea = document.getElementById("bleh");
+
+
 document.addEventListener("keydown", function(e){
+  var anotherAlien = document.getElementById("alien2");
+  var aX = Number(anotherAlien.getAttribute("x"));
+  var aY = Number(anotherAlien.getAttribute("y"));
+
+  var ufoThing = document.getElementById("ufo");
+  var uX = Number(ufoThing.getAttribute("x")) + 32;
+  var uY = Number(ufoThing.getAttribute("y")) + 32;
+
+
+
   if(e.keyCode == 37){
     fs = fs - 15;
     flyingSaucer.setAttribute("x", fs);
-    console.log("fds");
+
   } else if(e.keyCode == 39){
     fs = fs + 15;
     flyingSaucer.setAttribute("x", fs);
@@ -20,4 +39,15 @@ document.addEventListener("keydown", function(e){
     fy = fy + 15;
     flyingSaucer.setAttribute("y", fy);
   }
+
+    if(uX < aX + 32 && uX > aX){
+      console.log("x-intercepted");
+      if(uY < aY + 32 && uY > aY){
+        console.log("y-intercepted");
+        foodEaten += 1;
+        console.log(foodEaten);
+        textArea.innerHTML = "Food Eaten: " + foodEaten;
+      }
+    }
+
 });
