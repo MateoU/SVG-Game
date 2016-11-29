f2a22819cc449c03132e4f0e30314025ac82cbca
trayveonte <tnichols456@gmail.com>
2016-11-29 10:56:28 -0800

    Added timer feature

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var timeStart = Date.now();
 var namespace = "http://www.w3.org/2000/svg"
 
 var flyingSaucer = document.getElementById("ufo");
@@ -8,9 +9,20 @@ function randomNumber(min,max) {
  return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-var foodEaten = 0;
+var timeThing = 0;
+var foodEaten = 10;
+var aliensSaved = 0;
+
 var textArea = document.getElementById("bleh");
 
+setInterval(minusOne, 1000);
+
+function minusOne() {
+  foodEaten -= 1;
+  textArea.innerHTML = "Score: " + foodEaten;
+  if(foodEaten == 0){endGame()}
+  timeThing += 1;
+}
 
 document.addEventListener("keydown", function(e){
   var anotherAlien = document.getElementById("alien2");
@@ -41,13 +53,33 @@ document.addEventListener("keydown", function(e){
   }
 
     if(uX < aX + 32 && uX > aX){
-      console.log("x-intercepted");
+      //console.log("x-intercepted");
       if(uY < aY + 32 && uY > aY){
-        console.log("y-intercepted");
-        foodEaten += 1;
-        console.log(foodEaten);
-        textArea.innerHTML = "Food Eaten: " + foodEaten;
+        //console.log("y-intercepted");
+        foodEaten += 3;
+        //console.log(foodEaten);
+        textArea.innerHTML = "Score: " + foodEaten;
+        var newXPos = randomNumber(100, 978);
+        anotherAlien.setAttribute("x", newXPos);
+        anotherAlien.setAttribute("y", 0);
+        aliensSaved += 1;
       }
     }
-
+    if(foodEaten == 0){endGame()}
 });
+
+function endGame(){
+
+    document.getElementById("thing").pauseAnimations()
+    var timeStop = Date.now();
+    var newThing = timeStop - timeStart;
+    var newnewThing = newThing / 1000;
+    var new3Thing = Math.round(newnewThing);
+    $("#slideDown").slideDown();
+    $("#slideDown").css("display", "block");
+    document.getElementById("middleThing").innerHTML = "You saved " + aliensSaved + " aliens." + "<br>" + "You survived for " + timeThing + " seconds.";
+}
+
+function reloadPage() {
+    location.reload();
+}
